fa3fab069593ac876ac7dbb1477ae6f392a79f7c
oshanamarathunga <oshanamarathunga1@gmail.com>
2025-06-14 00:41:28 +0530

    Revert "bugfix/oshan/b4"
    This reverts commit 524194dbd4df012f5e1414eb5113279568d0fdae.

--- src/main/java/com/example/demo/test/dto.java
+++ src/main/java/com/example/demo/test/dto.java
@@ -2,5 +2,11 @@ package com.example.demo.test;
 
 public class dto {
     private String oshan;
+    private int number;
+    private String age;
+    private Boolean active;
 
+    Float cal;
+
+    private String bugFix;
 }
